f40cbab50ffb7d9514777626cc63f6ed5bc67db9
theo.chen <theo.chen@codebase.codes>
2021-05-13 13:56:05 +0800

    feat: add swiftformat

--- Example/PhobosSwiftExample/ViewController.swift
+++ Example/PhobosSwiftExample/ViewController.swift
@@ -1,24 +1,41 @@
 //
+//
 //  ViewController.swift
 //  PhobosSwiftExample
 //
-//  Created by Theo Chen on 2021/5/12.
+//  Copyright (c) 2021 Restless Codes Team (https://github.com/restlesscode/)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
-import UIKit
 import PhobosSwiftCore
+import UIKit
 
 class ViewController: UIViewController {
-
     let core = PBSCore.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+
         core.checkInternalVersion { needUpgrade, previousVersion, currentVersion in
             print(needUpgrade, previousVersion.string, currentVersion.string)
         }
     }
 }
-
