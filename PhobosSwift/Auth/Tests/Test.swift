a5f275e79ca4bc3f79716f831994c3bf4c6533e8
Restless Developer <developer@codebase.codes>
2021-05-17 12:29:54 +0800

    feat: add Auth framework

---
+++ PhobosSwift/Auth/Tests/Test.swift
@@ -0,0 +1,14 @@
+import Foundation
+import XCTest
+
+class LogTest: XCTestCase {
+  override func setUp() {
+    super.setUp()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+  }
+
+  func testCases() {}
+}
