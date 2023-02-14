33979966b17e36149edd022a9523ca858dc26870
ThorFixHero <xu_wendajxq@163.com>
2023-02-14 21:16:34 +0800

    fix: compile error for Xcode 14

--- PhobosSwift/UIComponent/Classes/Privatized+third+party+code/Charts/Components/Marker.swift
+++ PhobosSwift/UIComponent/Classes/Privatized+third+party+code/Charts/Components/Marker.swift
@@ -12,8 +12,8 @@
 import CoreGraphics
 import Foundation
 
-@objc(IChartMarker)
-public protocol IMarker: AnyObject {
+@objc(ChartMarker)
+public protocol Marker: AnyObject {
   /// - Returns: The desired (general) offset you wish the IMarker to have on the x-axis.
   /// By returning x: -(width / 2) you will center the IMarker horizontally.
   /// By returning y: -(height / 2) you will center the IMarker vertically.
@@ -26,13 +26,13 @@ public protocol IMarker: AnyObject {
   ///            If you have no adjustments to make, return self.offset().
   func offsetForDrawing(atPoint: CGPoint) -> CGPoint
 
-  /// This method enables a custom IMarker to update it's content every time the IMarker is redrawn according to the data entry it points to.
+  /// This method enables a custom Marker to update it's content every time the Marker is redrawn according to the data entry it points to.
   ///
   /// - Parameters:
   ///   - entry: The Entry the IMarker belongs to. This can also be any subclass of Entry, like BarEntry or CandleEntry, simply cast it at runtime.
   ///   - highlight: The highlight object contains information about the highlighted value such as it's dataset-index, the selected range or stack-index (only stacked bar entries).
   func refreshContent(entry: ChartDataEntry, highlight: Highlight)
 
-  /// Draws the IMarker on the given position on the given context
+  /// Draws the Marker on the given position on the given context
   func draw(context: CGContext, point: CGPoint)
 }
--- PhobosSwift/UIComponent/Classes/Privatized+third+party+code/Charts/Data/Interfaces/BarChartDataSetProtocol.swift
+++ PhobosSwift/UIComponent/Classes/Privatized+third+party+code/Charts/Data/Interfaces/BarChartDataSetProtocol.swift
@@ -1,5 +1,5 @@
 //
-//  IBarChartDataSet.swift
+//  BarChartDataSetProtocol.swift
 //  Charts
 //
 //  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
@@ -13,7 +13,7 @@ import CoreGraphics
 import Foundation
 
 @objc
-public protocol IBarChartDataSet: IBarLineScatterCandleBubbleChartDataSet {
+public protocol BarChartDataSetProtocol: BarLineScatterCandleBubbleChartDataSetProtocol {
   // MARK: - Data functions and accessors
 
   // MARK: - Styling functions and accessors
